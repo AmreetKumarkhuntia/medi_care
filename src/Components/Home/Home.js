0e8caba5b01e497718298fb731009cd9391d0e67
AmreetKumarkhuntia <amreetleo123@gmail.com>
2022-09-04 15:05:54 +0530

    home update

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -4,12 +4,50 @@ import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
 import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 import Button from '@mui/material/Button';
 
+import {useState} from 'react';
+
 const Home = () => {
+    const [pin,setPin] = useState(753012);
+    const [city,setCity] = useState('');
+    const [name,setName] = useState('');
+    const [district,setDistrict] = useState('');
+    const [state,setState] = useState('');
+
+    const [hospital,setHospital] = useState([]);
+
+    
+    
+    function hospitalMap(hospital){
+        return (
+            <div className="hospital">
+                <h2>{hospital.name}</h2>
+                <div>Contact No.= {hospital.contactno}</div>
+                <div>City Name= {hospital.city}</div>
+                <div>State= {hospital.state}</div>
+                <div>District= {hospital.district}</div>
+                <div>Pincode= {hospital.pincode}</div>
+                <div>Country= {hospital.country}</div>
+            </div>
+        );
+    }
+
+    async function  fetchhospital(){
+        let url="http://localhost:80/hospital"
+
+        fetch(url+'?city='+city+'&pin='+pin+'&name='+name+'&district='+district+'&state='+state).then(
+            (res)=>{
+                return res.json();
+            }
+        ).then((res)=>{
+            console.log(res.hospitals);
+            setHospital(res.hospitals);
+        })
+    }
+
     return ( <div className="Home">
         <div className='heading_container'>
             <div className='left'>
@@ -25,50 +63,56 @@ const Home = () => {
                 
             </div>
         </div>
+
         <div className="form_showcase"> 
             <h1>Search Hospitals</h1>
-            <TableContainer sx={{ width: 400 }}  component={Paper}>
+            <TableContainer sx={{ width: 350 }}  component={Paper}>
                 <Table aria-label="simple table">
-                    <TableHead>
+                <TableBody>
                         <TableRow>
                             <TableCell>Name</TableCell>
                             <TableCell>
-                                <input type="text"></input>
+                                <input type="text" onChange={(e)=>{setName(e.target.value)}} ></input>
                             </TableCell>
                         </TableRow>
                         <TableRow>
                             <TableCell>City</TableCell>
                             <TableCell>
-                                <input type="text"></input>
+                                <input type="text" onChange={(e)=>{setCity(e.target.value)}}></input>
                             </TableCell>
                         </TableRow>
                         <TableRow>
                             <TableCell>District</TableCell>
                             <TableCell>
-                            <input type="text"></input>
+                            <input type="text" onChange={(e)=>{setDistrict(e.target.value)}}></input>
                             </TableCell>
                         </TableRow>
                         <TableRow>
                             <TableCell>State</TableCell>
                             <TableCell>
-                            <input type="text"></input>
+                            <input type="text" onchange={(e)=>{setState(e.target.value)}}></input>
                             </TableCell>
                         </TableRow>
                         <TableRow>
                             <TableCell>Pincode</TableCell>
                             <TableCell>
-                            <input type="text"></input>
+                            <input type="text" onChange={(e)=>{setPin(e.target.value)}}></input>
                             </TableCell>
                         </TableRow>
-                    </TableHead>
-                    <TableBody>
-
                     </TableBody>
                 </Table>
             </TableContainer>
-            <Button variant="contained">SUBMIT</Button>
+            <Button variant="contained" onClick={()=>{fetchhospital()}}>SUBMIT</Button>
+            
 
                 
+        </div>
+
+        <div className="show_hospital">
+            <h1>We got:</h1>
+            {
+                hospital.map(hospitalMap)
+            }
         </div>
     </div>);
 }
